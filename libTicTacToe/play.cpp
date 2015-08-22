715979a23444efaf286e7ff8dc42717b23dab278
oclancy <oclancy@gmail.com>
2015-08-22 14:05:40 +0100

    Load implemented Play against machine implemented

---
+++ libTicTacToe/play.cpp
@@ -0,0 +1,46 @@
+
+#include "stdafx.h"
+#include "libTicTacToe.h"
+#include "Player.h"
+#include "Episode.h"
+#include "QlearningIntelligence.h"
+#include <memory>
+
+namespace libTicTacToe
+{
+void play()
+{
+	system("cls");
+
+	std::unique_ptr<CPlayer> player1;
+	std::unique_ptr<CPlayer> player2;
+
+	CQLearningIntelligence qIntelligence;
+	std::cout << "Please enter filename for intelligence. (enter 'none' for no file load)";
+	
+	std::string filename;
+	std::cin >> filename;
+	if(filename != "none")
+		qIntelligence.Load(filename);
+
+	std::cout << "You are 'x'. Would you like to go 1st or 2nd? (enter 1 or 2)";
+	int response;
+	std::cin >> response;
+
+	if (response == 1)
+	{
+		player1.reset(new CManualPlayer('x'));
+		player2.reset(new CAutoPlayer('o', qIntelligence));
+	}
+	else
+	{
+		player2.reset(new CManualPlayer('x'));
+		player1.reset(new CAutoPlayer('o', qIntelligence));
+	}
+
+	CEpisode::Play(*player1, *player2);
+
+	getchar();
+	std::cin.get();
+}
+}
